2fc085710ede0148f3fc1c05b922f209558331ca
Aditya Prerepa <43913660+AdiBaby@users.noreply.github.com>
2018-11-09 22:48:25 -0800

    Create ChatClientThread.java

---
+++ JavaNetworking_FrontEnd/ChatClientThread.java
@@ -0,0 +1,43 @@
+import java.net.*;
+import java.io.*;
+
+public class ChatClientThread extends Thread {
+    private Socket socket;
+    private ChatClient client;
+    private DataInputStream streamIn;
+
+    public ChatClientThread(ChatClient client, Socket socket) {
+        this.client = client;
+        this.socket  = socket;
+        open();
+        start();
+    }
+
+    public void open() {
+        try {
+            streamIn  = new DataInputStream(socket.getInputStream());
+        } catch(IOException ioe) {
+            System.out.println("Error getting input stream: " + ioe);
+            client.stop();
+        }
+    }
+
+    public void close() {
+        try {
+            if (streamIn != null) streamIn.close();
+        }catch(IOException ioe) {
+            System.out.println("Error closing input stream: " + ioe);
+        }
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                client.handle(streamIn.readUTF());
+            } catch(IOException ioe) {
+                System.out.println("Listening error: " + ioe.getMessage());
+                client.stop();
+            }
+        }
+    }
+}
