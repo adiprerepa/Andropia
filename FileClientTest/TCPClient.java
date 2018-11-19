fc81d5f4a7d4d8383d041ac34837f3fe1b5ab763
Aditya Prerepa <43913660+AdiBaby@users.noreply.github.com>
2018-11-18 19:13:30 -0800

    TCPClient.java

---
+++ FileClientTest/TCPClient.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.net.*;
+
+class TCPClient {
+
+    private final static String serverIP = "71.198.166.228";
+    private final static int serverPort = 55555;
+    private final static String fileOutput = "/home/ubuntu/test/new.txt";
+
+    public static void main(String args[]) {
+        byte[] aByte = new byte[1];
+        int bytesRead;
+
+        Socket clientSocket = null;
+        InputStream is = null;
+
+        try {
+            clientSocket = new Socket( serverIP , serverPort );
+            is = clientSocket.getInputStream();
+        } catch (IOException ex) {
+            // Do exception handling
+        }
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        if (is != null) {
+
+            FileOutputStream fos = null;
+            BufferedOutputStream bos = null;
+            try {
+                fos = new FileOutputStream( fileOutput );
+                bos = new BufferedOutputStream(fos);
+                bytesRead = is.read(aByte, 0, aByte.length);
+
+                do {
+                        baos.write(aByte);
+                        bytesRead = is.read(aByte);
+                } while (bytesRead != -1);
+
+                bos.write(baos.toByteArray());
+                bos.flush();
+                bos.close();
+                clientSocket.close();
+            } catch (IOException ex) {
+                // Do exception handling
+            }
+        }
+    }
+}
